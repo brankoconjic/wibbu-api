d0fa62f03fa57e5343760eab07d3828a93cd9c0a
Branko Conjic <brankoconjic@gmail.com>
2023-05-23 14:31:16 +0200

    Exit application if DATABASE_URL is missing

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,15 @@
-export const DATABASE_URL = process.env as Record<string, string>;
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file.
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+	console.error('Missing environment variable DATABASE_URL. Set this variable in .env file and restart the application.');
+	process.exit(1);
+}
+
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const PORT = process.env.PORT !== undefined ? parseInt(process.env.PORT, 10) : 3300;
+
 export const API_VERSION = 'v1' as const;
 export const API_PREFIX = `api/${API_VERSION}` as const;
-export const PORT = process.env.PORT !== undefined ? parseInt(process.env.PORT, 10) : 3300;
